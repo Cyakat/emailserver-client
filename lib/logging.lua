2695e8fcba370f1c8b6e0ba355552af71cc9a41a
Noah Bauer <147219@msa.asd20.org>
2020-02-21 09:57:14 -0700

    changed debugLog to logging and made it to match the python logging library.

--- lib/logging.lua
+++ lib/logging.lua
@@ -1,45 +1,84 @@
 local logging = {}
 
-logging.debugOn = true
-logging.infoOn = true
-logging.warningOn = true
-logging. warnOn = true
-logging.errorOn = true
-logging.criticalOn = true
+function logging.level(level)
+  if level = "debug" then
+    logging.debugOn = true
+    logging.infoOn = true
+    logging.warningOn = true
+    logging.warnOn = true
+    logging.errorOn = true
+    logging.criticalOn = true
+  elseif level = "info" then
+    logging.debugOn = false
+    logging.infoOn = true
+    logging.warningOn = true
+    logging.warnOn = true
+    logging.errorOn = true
+    logging.criticalOn = true
+  elseif level = "warn" then
+    logging.debugOn = false
+    logging.infoOn = false
+    logging.warningOn = true
+    logging.warnOn = true
+    logging.errorOn = true
+    logging.criticalOn = true
+  elseif level = "warning" then
+    logging.debugOn = false
+    logging.infoOn = false
+    logging.warningOn = true
+    logging.warnOn = false
+    logging.errorOn = true
+    logging.criticalOn = true
+  elseif level = "error" then
+    logging.debugOn = false
+    logging.infoOn = false
+    logging.warningOn = false
+    logging.warnOn = false
+    logging.errorOn = true
+    logging.criticalOn = true
+  elseif level = "critical" then
+    logging.debugOn = false
+    logging.infoOn = false
+    logging.warningOn = false
+    logging.warnOn = false
+    logging.errorOn = false
+    logging.criticalOn = true
+  end
+end
 
 function logging.debug(string)
   if debugOn then
-    print(string)
+    print("DEBUG: "..string)
   end
 end
 
 function logging.info(string)
   if infoOn then
-    print(string)
+    print("INFO: "..string)
   end
 end
 
 function logging.warning(string)
   if warningOn then
-    print(string)
+    print("WARNING: "..string)
   end
 end
 
 function logging.warn(string)
   if warnOn then
-    print(string)
+    print("WARN: "..string)
   end
 end
 
 function logging.error(string)
   if errorOn then
-    pring(string)
+    pring("ERROR: "..string)
   end
 end
 
 function logging.critical(string)
   if criticalOn then
-    print(string)
+    print("CRITICAL: "..string)
   end
 end
 
